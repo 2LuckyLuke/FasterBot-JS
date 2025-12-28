2277d909c418507874464245c860b097e3e1c9c0
Luke Weetendorf <luke.weetendorf@protonmail.com>
2025-12-28 12:16:49 +0100

    include intent to read message contents

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.GuildModeration,
     GatewayIntentBits.GuildExpressions,
