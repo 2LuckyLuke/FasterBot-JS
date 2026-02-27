3ee8e58cdef1c0ab158576fcbdc3a12e549fb3b1
Luke Weetendorf <luke.weetendorf@protonmail.com>
2026-02-27 03:10:16 +0100

    add additional check if channel exists

--- src/commands/voice/updateTextChannelVisibilty.ts
+++ src/commands/voice/updateTextChannelVisibilty.ts
@@ -1,7 +1,8 @@
 import { ChannelType, GuildBasedChannel } from "discord.js";
 
 export function updateTextChannelVisibility(memberId: string, channelVisibility: 'visible' | 'hidden', channel?: GuildBasedChannel,): void {
-    if (channel !== undefined && channel.type === ChannelType.GuildText && memberId !== undefined) {
+    const channelExists = channel !== null && channel !== undefined;
+    if (channelExists && channel.type === ChannelType.GuildText && memberId !== undefined) {
         channelVisibility === 'visible' ?
             channel.permissionOverwrites.create(memberId, { ViewChannel: true }) :
             channel.permissionOverwrites.delete(memberId);
